c5994bc3f88d8f0e10636a4da78696d17f4f7c72
SD91PL <sd91pl@gmail.com>
2023-12-09 17:16:51 +0100

    added navigation and main sections with blur animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+	// Home
 	const home = document.querySelector('.home')
-	const homeLogo = document.querySelector('.home__logo')
 	const unHome = document.querySelector('.under-home')
 	const startBtn = document.querySelector('.start-btn')
 
@@ -13,4 +13,53 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	startBtn.addEventListener('click', hideHome)
+
+	// Nav - Main Sections - Blur Animation
+	const sOne = document.querySelector('.s-one')
+	const sTwo = document.querySelector('.s-two')
+	const sThree = document.querySelector('.s-three')
+	const nOne = document.querySelector('.n-one')
+	const nTwo = document.querySelector('.n-two')
+	const nThree = document.querySelector('.n-three')
+
+	const showOne = () => {
+		sOne.classList.remove('focus-in')
+		sOne.classList.remove('blur-out')
+		sTwo.classList.add('blur-out')
+		sThree.classList.add('blur-out')
+		setTimeout(() => {
+			sTwo.classList.add('hidden')
+			sThree.classList.add('hidden')
+		}, 900)
+		sOne.classList.add('focus-in')
+		sOne.classList.remove('hidden')
+	}
+	const showTwo = () => {
+		sTwo.classList.remove('focus-in')
+		sTwo.classList.remove('blur-out')
+		sOne.classList.add('blur-out')
+		sThree.classList.add('blur-out')
+		setTimeout(() => {
+			sOne.classList.add('hidden')
+			sThree.classList.add('hidden')
+		}, 900)
+		sTwo.classList.add('focus-in')
+		sTwo.classList.remove('hidden')
+	}
+	const showThree = () => {
+		sThree.classList.remove('focus-in')
+		sThree.classList.remove('blur-out')
+		sOne.classList.add('blur-out')
+		sTwo.classList.add('blur-out')
+		setTimeout(() => {
+			sOne.classList.add('hidden')
+			sTwo.classList.add('hidden')
+		}, 900)
+		sThree.classList.add('focus-in')
+		sThree.classList.remove('hidden')
+	}
+
+	nOne.addEventListener('click', showOne)
+	nTwo.addEventListener('click', showTwo)
+	nThree.addEventListener('click', showThree)
 })
