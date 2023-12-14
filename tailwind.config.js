18133a852085894ee5d4dc72525128363829d8b5
SD91PL <sd91pl@gmail.com>
2023-12-14 22:38:39 +0100

    changed breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
 		container: {
 			center: true,
 		},
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '940px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		  }
 	},
 	plugins: [],
 }
