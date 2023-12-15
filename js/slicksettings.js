c58be81372f9a5c041b24581488f6593ea3d1b2c
SD91PL <sd91pl@gmail.com>
2023-12-15 11:45:56 +0100

    moved slicksettings to main.js

--- js/slicksettings.js
+++ js/slicksettings.js
@@ -1,16 +1,1 @@
-$('.topics-carousel').slick({
-	arrows: false,
-	autoplay: true,
-	autoplaySpeed: 3200,
-	mobileFirst: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 3,
-			},
-		},
-	],
-})
+
