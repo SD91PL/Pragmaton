b5e937eb7d3fc64dd041ace06d49ea95b7cf2dcd
SD91PL <sd91pl@gmail.com>
2023-12-21 21:23:48 +0100

    added a lite-youtube element instead of an iframe

--- js/content.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	// Embed Youtube Video
-	let iframe = document.querySelector('.iframe')
-	setTimeout(() => {
-		iframe.src = 'https://www.youtube.com/embed/Czp8-Q1Ylco?si=bTAAXHd68N6mnRsq'
-	}, 1200)
-})
-
