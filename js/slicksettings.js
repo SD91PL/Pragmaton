4676e80884e56832bb8d71f94df0b0229a5bb93c
SD91PL <sd91pl@gmail.com>
2023-12-21 09:16:47 +0100

    2023-12-21 - version 1.3 - optimized css, img, js

---
+++ js/slicksettings.js
@@ -0,0 +1,22 @@
+$('.topics-carousel').slick({
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 3200,
+	mobileFirst: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+		{
+			breakpoint: 2500,
+			settings: {
+				slidesToShow: 5,
+			},
+		},
+	],
+})
