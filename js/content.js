218321d5a4e77fc175841b46be1ec362c3fe4eed
SD91PL <sd91pl@gmail.com>
2024-03-07 21:50:28 +0100

    added video to content.js

--- js/content.js
+++ js/content.js
@@ -1,6 +1,6 @@
 const liteYT = document.querySelector('lite-youtube')
-const titles = ['Teraz albo nigdy', 'Wniosek']
-const videos = ['Czp8-Q1Ylco', 'QAUWpVuOZ4s']
+const titles = ['Jestem', 'Teraz albo nigdy', 'Wniosek']
+const videos = ['Stx494d7xYE','Czp8-Q1Ylco', 'QAUWpVuOZ4s']
 
 const generateVideoID = () => {
 	const randomIndex = Math.floor(Math.random() * (titles.length - 0.0001))
